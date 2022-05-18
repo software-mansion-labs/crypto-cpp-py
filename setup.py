42b0cef766b7441449548b6cd05af7fed28b43b4
Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>
2022-05-18 08:11:05 +0200

    Add bindings code and build scripts

---
+++ setup.py
@@ -0,0 +1,64 @@
+import subprocess
+import os
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
+from setuptools.command.build_py import build_py
+
+
+class CryptoExtension(Extension):
+    def __init__(self):
+        self.name = "crypto_cpp"
+        Extension.__init__(self, self.name, sources=[])
+
+
+class BuildPy(build_py):
+    def run(self):
+        self.run_command("build_ext")
+        return super().run()
+
+
+class BuildCrypto(build_ext):
+    already_built = False
+
+    def build_extension(self, ext):
+        if self.already_built:
+            print("was already built, skipping...")
+            return
+
+        if os.name != "nt":
+            subprocess.run("chmod a+x ./build_extension.sh", shell=True, check=True)
+            subprocess.run(
+                "./build_extension.sh",
+                shell=True,
+                check=True,
+            )
+        else:
+            with subprocess.Popen(
+                ["powershell.exe", ".\\build_extension.ps1"]
+            ) as process:
+                process.wait()
+                if process.returncode != 0:
+                    raise Exception("Build returned a non-zero code")
+
+        self.already_built = True
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+setup(
+    name="crypto_cpp_py",
+    version="1.0.0",
+    description="This is a packaged crypto-cpp program",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    ext_modules=[CryptoExtension()],
+    cmdclass={
+        "build_py": BuildPy,
+        "build_ext": BuildCrypto
+    },
+    python_requires=">=3.7.2,<3.10",
+    packages=["crypto_cpp_py"],
+    package_data={
+        "crypto_cpp_py": ["../libcrypto_c_exports.*"]
+    }
+)
