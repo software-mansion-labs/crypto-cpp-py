93cfd50827800b42aa586baf8c7a497b5c52852c
ddoktorski <45050160+ddoktorski@users.noreply.github.com>
2026-04-03 12:03:19 +0200

    v2.0.0 (#29)
    * Update supported python versions
    
    * v2.0.0
    
    * Update python version in package.yml
    
    * Fix build workflow
    
    * Fix ubuntu build
    
    * Fix for all linux os
    
    * Add apt-get installation
    
    * Drop support for legacy linux 32-bit archs
    
    * Apply changes only to linux
    
    * Update README

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open("README.rst") as f:
 
 setup(
     name="crypto_cpp_py",
-    version="1.4.5",
+    version="2.0.0",
     description="This is a packaged crypto-cpp program",
     author="Wojciech Szymczyk, Marcin Warchoł, Kamil Jankowski",
     author_email="wojciech.szymczyk@swmansion.com, marcin.warchol@swmansion.com, kamil.jankowski@swmansion.com",
